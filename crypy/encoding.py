c229beb0b4af8f6fd2fc70c1a7a56204ca0886b9
DerbelSahar <derbel.sahar@insat.u-carthage.tn>
2020-12-31 15:38:22 +0100

    encoding

--- crypy/encoding.py
+++ crypy/encoding.py
@@ -1,2 +1,17 @@
+from .utils import Menu
+import base64
 def encode_decode():
-    pass
+    Menu([
+        ("encoding a message", encode),
+        ("decoding a code", decode),
+    ]).run()
+
+def encode():
+    message = input("enter the message to encode")
+    code = base64.b64encode(message.encode('ascii'))
+    print(code.decode('ascii'))
+
+def decode():
+    code = input("enter the code to decode")
+    message = base64.b64decode(code.encode('ascii'))
+    print(message.decode('ascii'))
