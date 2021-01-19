b6190ae8ed0fa6e175fbb45056df06deef3644ce
Alaeddine Abdessalem <alaeddine-13@live.fr>
2021-01-19 04:31:04 +0100

    WIP: Adding dependency injection and services

--- crypy/utils.py
+++ crypy/utils.py
@@ -3,15 +3,21 @@ from typing import Optional
 import sys
 from operator import itemgetter
 from abc import ABC, abstractmethod, ABCMeta
+import uuid
 
+import streamlit as st
+
+def rerun():
+    raise st.script_runner.RerunException(st.script_request_queue.RerunData(None))
 
 class Menu(ABC):
     choices = []
     
     '''Display a menu and respond to choices when run.'''
     def __init__(self, choices: Optional[Iterable] = None, include_quit: bool = True,
-                 once: bool = True, choice_message: str = "Enter an option: ",
-                 quit_message: Optional[str] = "Quitting", include_back: bool = False
+                 once: bool = True, choice_message: str = "Choose an option: ",
+                 quit_message: Optional[str] = "Quitting", include_back: bool = False,
+                 mode: str = ''
     ):
         if not choices:
             choices = []
@@ -20,22 +26,27 @@ class Menu(ABC):
         self.choice_message = choice_message
         self.include_back = include_back
         self.include_quit = include_quit
-
-        if self.include_back:
-            self.choices.append(("Back", lambda : "GO_BACK"))
-        
-        if self.include_quit:
-            self.choices.append(("Quit", self.quit))
         
         self.quit_message = quit_message
+        self.mode = mode
+    
+    @property
+    def items(self):
+        result = self.choices.copy()
+        if self.include_quit:
+            result.append(("Quit", self.quit))
+
+        if self.include_back:
+            result.append(("Back", lambda : "GO_BACK"))
+        return result
     
     @property
     def actions(self):
-        return list(map(itemgetter(1), self.choices))
+        return list(map(itemgetter(1), self.items))
     
     @property
     def descriptions(self):
-        return list(map(itemgetter(0), self.choices))
+        return list(map(itemgetter(0), self.items))
 
     @abstractmethod
     def display_menu(self):
@@ -50,7 +61,41 @@ class Menu(ABC):
         pass
 
 class StreamlitMenu(Menu):
-    pass
+    @property
+    def placeholder(self):
+        if self.mode == 'sidebar':
+            return st.sidebar
+        else:
+            return st
+
+    def display_menu(self) -> int:
+        option = self.placeholder.selectbox(
+            self.choice_message, list(enumerate(self.descriptions, start=1)),
+            format_func=itemgetter(1)
+        )[0]
+        return option
+    
+    def run(self):
+        '''Display the menu and respond to choices.'''
+        while True:
+            choice = self.display_menu()
+            action = self.actions[choice-1]
+            if action:
+                result = action()
+                if result == "GO_BACK":
+                    break
+            if self.once:
+                return result
+        return result
+    
+    @property
+    def items(self):
+        return self.choices
+
+    def quit(self):
+        self.placeholder.write("Terminated")
+        if not self.placeholder.button("Restart"):
+            st.stop()
 
 class CLIMenu(Menu):
     def display_menu(self):
@@ -68,7 +113,7 @@ class CLIMenu(Menu):
         while True:
             self.display_menu()
             try:
-                choice = int(input("Enter an option: "))
+                choice = int(input(self.choice_message + "\n"))
                 action = self.actions[choice-1]
             except:
                 print("Invalid input, retry...")
@@ -93,6 +138,17 @@ class MenuProvider(ABC):
     def provide_menu(self) -> Menu:
         pass
 
+class Color:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 
 if __name__ == "__main__":
-    Menu([("hello", lambda: print("hello"))], once=True, include_quit=True, include_back=False).run()
+    CLIMenu([("hello", lambda: print("hello"))], once=True, include_quit=True, include_back=False).run()
