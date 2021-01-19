2de19199263126b4efd0fc660c34025db6dc5888
Alaeddine Abdessalem <alaeddine-13@live.fr>
2021-01-19 10:53:53 +0100

    Suppress DuplicateWidgetID error

--- st_app.py
+++ st_app.py
@@ -3,10 +3,14 @@ from crypy import main, container
 import crypy
 from crypy.services import StreamlitService
 from dependency_injector import containers, providers
+from streamlit.errors import DuplicateWidgetID
 import sys
 
 
 container.wire(modules=[sys.modules[__name__]], packages=[crypy])
 with container.service.override(providers.Factory(StreamlitService)):
-    main()
+    try:
+        main()
+    except DuplicateWidgetID as e:
+        print(e)
 
